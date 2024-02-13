5c62a48ecf53262db75dac4d2d1c8816b302992f
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-02-12 23:00:09 -0800

    blog works

--- blog_index.py
+++ blog_index.py
@@ -4,16 +4,17 @@ import os
 with open("blog/a_blog.html") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 f = open("blog_index.html","w")
-f.write(r"<head>")
-f.write(r"    <title>gay raccoon girl on the web</title>")
-f.write(r"    <link rel=\"stylesheet\" href=\"styling.css\">")
-f.write(r"</head>")
+f.write("<head>"+"\n")
+f.write("    <title>gay raccoon girl on the web</title>"+"\n")
+f.write("    <link rel=\"stylesheet\" href=\"styling.css\">"+"\n")
+f.write("</head>"+"\n")
+f.write("<h1> here are all of my blog posts</h1>")
 
 directory = os.fsdecode("blog")
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     with open("blog/" + filename) as fp2:
         soup2 = BeautifulSoup(fp2, 'html.parser')
-    f.write("<a href=\"" + "blog/" + filename + str(soup2.h1) + "</a>")
+    f.write("<a href=\"" + "blog/" + filename + "\">" + str(soup2.h1.text) + "</a>"+"<br>")
 #f.write(str(soup.h1))
 f.close()

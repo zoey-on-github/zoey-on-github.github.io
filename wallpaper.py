87b656461b0b8d111cd0e51fa290ea744f3ac2e0
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-03-25 02:21:44 -0700

    wallpapers page and script works now

--- wallpaper.py
+++ wallpaper.py
@@ -1,11 +1,10 @@
-fom bs4 import BeautifulSoup
 import os
 
-directory = os.fsdecode("blog")
+f = open("wallpapers.html", "w")
+
+directory = os.fsdecode("wallpapers")
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-    with open("wallpapers/" + filename) as fp2:
-        soup2 = BeautifulSoup(fp2, 'html.parser')
-    f.write("<a href=\"" + "blog/" + filename + "\">" + str(soup2.h1.text) + "</a>"+"<br>\n")
-#f.write(str(soup.h1))
+    # with open("wallpapers/" + filename) as fp2:
+    f.write("<img src=\"" + "wallpapers/" + filename + "\">" + "<br>\n")
 f.close()

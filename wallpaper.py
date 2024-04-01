2caa75551a731e2be72ee6a4fe1dee1032a45f14
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-03-31 17:54:29 -0700

    grid

--- wallpaper.py
+++ wallpaper.py
@@ -1,10 +1,10 @@
 import os
 
-f = open("wallpapers.html", "w")
-
+f = open("wallpapers.html", "w", encoding="utf-8")
+f.write("    <link rel=\"stylesheet\" href=\"styling.css\">"+"\n")
 directory = os.fsdecode("wallpapers")
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     # with open("wallpapers/" + filename) as fp2:
-    f.write("<img src=\"" + "wallpapers/" + filename + "\">" + "<br>\n")
+    f.write("<img src=\"" + "wallpapers/" + filename + "\">" + "\n")
 f.close()

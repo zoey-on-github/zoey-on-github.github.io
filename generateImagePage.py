2e6c79c86cb86276396a1263bbb549bbd61980fc
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-07-01 00:15:06 -0700

    redid python script, no longer need seperate scripts for every page

--- generateImagePage.py
+++ generateImagePage.py
@@ -1,10 +1,11 @@
 import os
+import sys
 
-f = open("wallpapers.html", "w", encoding="utf-8")
+f = open(sys.argv[1], "w", encoding="utf-8")
 f.write("    <link rel=\"stylesheet\" href=\"wallpaper.css\">"+"\n")
-directory = os.fsdecode("wallpapers")
+directory = os.fsdecode("yuri")
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     # with open("wallpapers/" + filename) as fp2:
-    f.write("<img src=\"" + "wallpapers/" + filename + "\">" + "\n")
+    f.write("<img src=\"" + sys.argv[2] + filename + "\">" + "\n")
 f.close()
